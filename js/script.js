9765322b1fc154fd69c0d72481da536d2caae8b8
patoliyaridham07-cloud <patoliyaridham07@gmail.com>
2026-03-10 16:47:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -192,3 +192,19 @@ backToTopBtn.addEventListener("click", () => {
   });
 
 });
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+
+  navLinks.classList.toggle("active");
+
+  if (navLinks.classList.contains("active")) {
+    hamburger.textContent = "✖";
+    hamburger.setAttribute("aria-expanded", "true");
+  } else {
+    hamburger.textContent = "☰";
+    hamburger.setAttribute("aria-expanded", "false");
+  }
+
+});
